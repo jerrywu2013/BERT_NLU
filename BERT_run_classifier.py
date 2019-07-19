4fbdb22cd6d211d898f6a65eaf78dcca755409f0
Jerry Wu <jerry@mail.ntust.edu.tw>
2019-07-19 18:46:54 +0800

    fine-tune-BERT

---
+++ BERT_run_classifier.py
@@ -0,0 +1,72 @@
+#run_classifier.py
+
+
+class C(DataProcessor):
+    """Processor for Demo data set."""
+
+    def __init__(self):
+        self.labels = set()
+    
+    def get_train_examples(self, data_dir):
+        """See base class."""
+        return self._create_examples(
+            self._read_tsv(os.path.join(data_dir, "train.tsv")), "train")
+
+    def get_dev_examples(self, data_dir):
+        """See base class."""
+        return self._create_examples(
+            self._read_tsv(os.path.join(data_dir, "dev.tsv")), "dev")
+
+    def get_test_examples(self, data_dir):
+      """See base class."""
+      return self._create_examples(
+          self._read_tsv(os.path.join(data_dir, "test.tsv")), "test")
+
+    def get_labels(self):
+        """See base class."""
+        # return list(self.labels)
+        return ["fashion", "houseliving","game"]
+
+
+    def _create_examples(self, lines, set_type):
+        """Creates examples for the training and dev sets."""
+        examples = []
+        for (i, line) in enumerate(lines):
+            guid = "%s-%s" % (set_type, i)
+            text_a = tokenization.convert_to_unicode(line[1])
+            label = tokenization.convert_to_unicode(line[0])
+            self.labels.add(label)
+            examples.append(
+                InputExample(guid=guid, text_a=text_a, text_b=None, label=label))
+        return examples
+
+
+# DemoProcessor
+
+  processors = {
+      "cola": ColaProcessor,
+      "mnli": MnliProcessor,
+      "mrpc": MrpcProcessor,
+      "xnli": XnliProcessor,
+      "demo": C,
+  }
+
+
+#Run
+
+export BERT_Chinese_DIR=/path/to/bert/chinese_L-12_H-768_A-12
+export Demo_DIR=/path/to/DemoDate
+
+python run_classifier.py \
+  --task_name=demo \
+  --do_train=true \
+  --do_eval=true \
+  --data_dir=$Demo_DIR \
+  --vocab_file=$BERT_Chinese_DIR/vocab.txt \
+  --bert_config_file=$BERT_Chinese_DIR/bert_config.json \
+  --init_checkpoint=$BERT_Chinese_DIR/bert_model.ckpt \
+  --max_seq_length=128 \
+  --train_batch_size=32 \
+  --learning_rate=2e-5 \
+  --num_train_epochs=3.0 \
+  --output_dir=/tmp/Demo_output/
